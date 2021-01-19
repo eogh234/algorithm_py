5b171ae0505331208216a51a148c85cfa04096db
Daeho <eogh234@naver.com>
2021-01-20 05:08:11 +0900

    1931

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-n, k = map(int, input().split())
-
-data = list()
-count = 0
+n = int(input())
+time = list()
+result = 0
 
 for i in range(n):
-    data.append(int(input()))
+    data = list(map(int, input().split()))
+    time.append(data)
 
-while k > 0:
-    for num in reversed(data):
-        if num > k:
-            continue
-        else:
-            count += int(k / num)
-            k -= (num * int(k / num))
+time.sort(key=lambda x: [x[1], x[0]])
 
-print(count)
+curEnd = -1
+for i in range(n):
+    if time[i][0] == time[i][1]:
+        result += 1
+        curEnd = time[i][1]
+    elif curEnd <= time[i][0]:
+        result += 1
+        curEnd = time[i][1]
+
+print(result)
