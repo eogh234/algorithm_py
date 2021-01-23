29d93e30edd89060c00c5fae85e812eac2151303
Daeho <eogh234@naver.com>
2021-01-24 03:49:12 +0900

    1012

--- main.py
+++ main.py
@@ -1,48 +1,44 @@
 from collections import deque
 
-n = int(input())
-
-graph = list()
-
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-result = []
-
 dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dy = [0, 0, 1, -1]
 
 
-def bfs(x, y):
+def bfs(y, x, count):
     queue = deque()
-    queue.append([x, y])
-    graph[x][y] = 0
-    count = 1
+    queue.append([y, x])
+    graph[y][x] = 0
+
     while queue:
-        x, y = queue.popleft()
+        y, x = queue.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+            if nx < 0 or ny < 0 or nx >= m or ny >= n:
                 continue
-            if graph[nx][ny] == 0:
+            if graph[ny][nx] == 0:
                 continue
-            if graph[nx][ny] == 1:
-                count += 1
-                graph[nx][ny] = 0
-                queue.append([nx, ny])
-    return count
+            if graph[ny][nx] == 1:
+                graph[ny][nx] = 0
+                queue.append([ny, nx])
+    return count + 1
 
+t = int(input())
 
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            cnt = 0
-            cnt = bfs(i, j)
-            result.append(cnt)
+for _ in range(t):
+    cnt = 0
+    m, n, k = map(int, input().split())
 
-print(len(result))
-result.sort()
-for i in result:
-    print(i)
+    graph = [[0] * m for _ in range(n)]
+
+    for _ in range(k):
+        x, y = map(int, input().split())
+        graph[y][x] = 1
+
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                cnt = bfs(i, j, cnt)
+    print(cnt)
