ca096d4d2de209d602b3c1e549fddd6cda958f2b
Daeho <eogh234@naver.com>
2021-02-23 08:05:46 +0900

    12015

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-k = int(sys.stdin.readline().rstrip())
+data = list(map(int, sys.stdin.readline().rstrip().split()))
 
-start = 1
-end = k
-result = 0
+arr = [0]
 
-while start <= end:
-    mid = (start + end) // 2
-    count = 0
-    for i in range(1, n + 1):
-        count += min(int(mid / i), n)
-    if count < k:
-        start = mid + 1
+
+def binary_search(start, end, target):
+    while start < end:
+        mid = (start + end) // 2
+        if arr[mid] < target:
+            start = mid + 1
+        elif arr[mid] > target:
+            end = mid
+        else:
+            end = start = mid
+    return end
+
+
+for num in data:
+    if num > arr[-1]:
+        arr.append(num)
     else:
-        result = mid
-        end = mid - 1
+        start = 1
+        end = len(arr) - 1
+        idx = binary_search(start, end, num)
+        arr[idx] = num
 
-print(result)
+print(len(arr) - 1)
