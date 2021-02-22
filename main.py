5678824a141409ccd04bd2e3bfae12643b618d70
Daeho <eogh234@naver.com>
2021-02-23 05:38:52 +0900

    1300

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 import sys
 
-n, m = map(int, sys.stdin.readline().rstrip().split())
-data = list(map(int, sys.stdin.readline().rstrip().split()))
+n = int(sys.stdin.readline().rstrip())
+k = int(sys.stdin.readline().rstrip())
 
-start = 0
-end = max(data)
-
-data.sort(reverse=True)
+start = 1
+end = k
 result = 0
-while start <= end:
-    count = 0
-    mid = (start + end) // 2
-    for l in data:
-        if l <= mid:
-            break
-        else:
-            count += l - mid
 
-    if count < m:
-        end = mid - 1
+while start <= end:
+    mid = (start + end) // 2
+    count = 0
+    for i in range(1, n + 1):
+        count += min(int(mid / i), n)
+    if count < k:
+        start = mid + 1
     else:
         result = mid
-        start = mid + 1
+        end = mid - 1
 
 print(result)
